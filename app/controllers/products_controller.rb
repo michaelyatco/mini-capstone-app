2b4540d8021d472c21a9f0674e8e30a591569238
Michael Yatco <mjyatco@gmail.com>
2017-01-19 21:31:43 -0500

    I've uploaded a get/post route and an index route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,20 @@ class ProductsController < ApplicationController
     @product = Product.first
     render "products.html.erb"
   end
+
+  def new_product_form_method
+    render "new_product_form.html.erb"
+  end
+
+  def new_product_result_method
+    @product = Product.new(name: params[:input_name], price: params[:input_price], image: params[:input_image], description: params[:input_description])
+    @product.save
+    render "new_product_result.html.erb"
+  end
+
+  def index
+    @products = Product.all
+    render "index.html.erb" 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products", to: "products#products_method"
+
+  get "/new_product", to: "products#new_product_form_method"
+  post "/new_product_result", to: "products#new_product_result_method"
+
+  get "/recipes", to: "recipes#index"
 end
